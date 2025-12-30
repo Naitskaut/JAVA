2bf9e1c0d931b0949e53d8e363e54e99fbd8d138
esken <eskendirabraim@gmail.com>
2025-12-30 20:09:47 +0500

    first

--- Main.java
+++ Main.java
@@ -1,19 +1,82 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        Sport s1 = new Sport("Volleyball","outdoor",6);
-        Sport s2 = new Sport("Football","outdoor",5);
-        s1.info();
-        s2.info();
-        System.out.println();
-        Athlete a1 = new Athlete("Yeskendir", "Abraimov", "Fitness");
-        Athlete a2 = new Athlete("Amir", "Kapasov", "HobbyHorsing");
-        a1.info();
-        a2.info();
-        System.out.println();
-        SportClub b1 = new SportClub("Chess", 8, "Dias");
-        SportClub b2 = new SportClub("Diet", 1, "Agzam");
-        b1.info();
-        b2.info();
+        Scanner input = new Scanner(System.in);
+        ArrayList<Person> people = new ArrayList<>();
+        System.out.println("Members number: ");
+        int n =  input.nextInt();
+        input.nextLine();
+
+        for(int i=0; i<n; i++){
+            System.out.println("\nMember " + (i + 1));
+
+            System.out.print("Name: ");
+            String name = input.nextLine();
+
+            System.out.print("Surname: ");
+            String surname = input.nextLine();
+
+            System.out.print("Age: ");
+            int age = Integer.parseInt(input.nextLine());
+
+            System.out.print("Height (meters): ");
+            double height = Double.parseDouble(input.nextLine());
+
+
+            System.out.print("Sport type: ");
+            String sportType = input.nextLine();
+
+            Member member = new Member(name, surname, age, height, sportType);
+            people.add(member);
+        }
+
+        people.add(new SportClub("Volleyball Club", 60, "Aruzhan"));
+        people.add(new SportClub("Basketball Club", 45, "Dias"));
+        System.out.println("\nALL OBJECTS ");
+        for(Person p : people){
+            p.info();
+        }
+        people.sort(Comparator.comparing(Person::getName));
+
+        System.out.println("\nSorted: ");
+        for(Person p : people){
+            p.info();
+        }
+        System.out.println("\nSPORT CLUBS WITH 50+ MEMBERS");
+        for (Person p : people) {
+            if (p instanceof SportClub club && club.getMembers() >= 50) {
+                club.info();
+            }
+        }
+        System.out.println("\nSEARCH MEMBERS BY SPORT: Volleyball");
+        for (Person p : people) {
+            if (p instanceof Member member &&
+                    member.getSportType().equalsIgnoreCase("Volleyball")) {
+                member.info();
+            }
+        }
+        System.out.println("\nEQUALS TEST");
+        Member m1 = new Member("Ali", "Khan", 18, 1.75, "Football");
+        Member m2 = new Member("Ali", "Khan", 18, 1.75, "Football");
+
+        System.out.println(people.equals(people));
+
+        System.out.println("\nCOMPARING ENTERED MEMBERS");
+        for (int i = 0; i < people.size(); i++) {
+            for (int j = i + 1; j < people.size(); j++) {
+                if (people.get(i).equals(people.get(j))) {
+                    System.out.println("Objects are equal:");
+                    System.out.println(people.get(i));
+                    System.out.println(people.get(j));
+                }
+            }
+        }
     }
+
+
 
 }
